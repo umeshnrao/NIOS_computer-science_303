bfcb34eec10b70b55cb2807e4fbc92bf3a7be0f8
Umesh Rao <umeshnrao@gmail.com>
2026-03-11 23:08:24 +0530

    Practical Solutions

---
+++ practicals/practical5_conditional_operator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+	
+	int number; 
+	
+	cout << "Enter the number:";
+	cin >> number;
+	
+	    (number % 2 == 0) ? 
+        cout << "The number is Even" : 
+        cout << "The number is Odd";
+    
+}
